da35eef25e9d77cd008b73253dd83ca8cea26c6f
azufb <curiousazunas@gmail.com>
2021-09-04 23:50:28 +0900

    アプリケーション名修正

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App:React.VoidFunctionComponent  = () => {
   return (
     <div className="bg-blue-300 h-screen bg-opacity-50">
       <div className="p-6">
-        <p className="text-3xl font-semibold font-mono">全角数字・アルファベットチェッカー</p>
+        <p className="text-3xl font-semibold font-mono">全角チェッカー</p>
           <p className="my-5 font-mono">
             急いでテキストを入力しているとき、誤って数字やアルファベットを全角で入力してしまうことってありませんか？<br />
             そんなときは、この「全角チェッカー」を使って、全角になってしまっている数字やアルファベットを見つけて修正しましょう！
